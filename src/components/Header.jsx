68e142e7366d8f6be6e774ad69e1bfcd933ed4ff
Lucas Bernardini <lucas.bernardini97@gmail.com>
2020-08-18 15:49:16 -0400

    completion of header and menu modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,39 @@
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
-import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import React from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+
+import './styling/header.scss'
 
 const useStyles = makeStyles({
   list: {
     width: 250,
   },
   fullList: {
-    width: 'auto',
+    width: "auto",
   },
 });
 
 export default function Header() {
   const classes = useStyles();
   const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
     right: false,
   });
 
   const toggleDrawer = (anchor, open) => (event) => {
-    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
       return;
     }
 
@@ -34,45 +43,59 @@ export default function Header() {
   const list = (anchor) => (
     <div
       className={clsx(classes.list, {
-        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
-      })}
+        [classes.fullList]: anchor === "top" || anchor === "bottom",
+      }) }
+    
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
       <List>
-        {['About', 'Author'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemText primary={text} />
+            <a className="list__links" href="https://lucasbernardini.com/" target="_" >
+          <ListItem button >
+            <ListItemText primary="Author" />
           </ListItem>
-        ))}
+            </a>
+            <a className="list__links" href="https://github.com/LucasBernardini/Workout-Manager" target="__">
+          <ListItem button>
+            <ListItemText primary='Repo' />
+          </ListItem>
+          </a>
       </List>
       <Divider />
       <List>
-        {['Coming Soon:', 'Saved Workouts', 'Preset Workouts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+        {["Coming Soon:", "Saved Workouts", "Preset Workouts"].map(
+          (text, index) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          )
+        )}
       </List>
     </div>
   );
 
   return (
-    <div>
-      {['right'].map((anchor) => (
-        <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-          <SwipeableDrawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
-            onOpen={toggleDrawer(anchor, true)}
-          >
-            {list(anchor)}
-          </SwipeableDrawer>
-        </React.Fragment>
-      ))}
+    <div className="header">
+      <div className="header__container">
+        <h1 className="title" >Workout Tracker</h1>
+        <p className="title__sub">By Lucas Bernardini</p>
+      </div>
+      <div>
+        {["Menu"].map((anchor) => (
+          <React.Fragment key={anchor}>
+            <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+            <SwipeableDrawer
+              anchor={anchor}
+              open={state[anchor]}
+              onClose={toggleDrawer(anchor, false)}
+              onOpen={toggleDrawer(anchor, true)}
+            >
+              {list(anchor)}
+            </SwipeableDrawer>
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 }
