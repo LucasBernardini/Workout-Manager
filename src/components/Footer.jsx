c12d71794dc6b13d0720e69d8da732f26dae3fb9
Lucas Bernardini <lucas.bernardini97@gmail.com>
2020-08-18 16:02:08 -0400

    completion of footre

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './styling/footer.scss'
 
 export default function Footer() {
     return (
-        <div>
-            
+        <div className="footer">
+        <h2 className="footer__head">Any Questions or Comments? Contact <a className="link" href="mailto:lucas.bernardini97@gmail.com">Lucas</a></h2>
+            <p className="footer__p">Workout Tracker is used to save information about your next workout session. All data is saved to local storage. Future iterations of the application will implement login features to store your workouts.</p>
         </div>
     )
 }
